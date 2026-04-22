31331aed63788eb49a402f6393390448f59c0b89
Codwithmutahir <mutharsoomro13@gmail.com>
2026-04-22 22:17:28 +0500

    Remove multiple title guide files and related blog URLs from sitemap. This cleanup streamlines the sitemap and eliminates outdated content. Made-with: Cursor

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -21,58 +21,6 @@
             <priority>{{ $page['priority'] }}</priority>
         </url>
     @endforeach
-
-    <!-- Blog Posts -->
-    @foreach ($blogs as $blog)
-        <url>
-            <loc>{{ url('/blog/' . $blog->slug) }}</loc>
-            <lastmod>{{ $blog->updated_at->toAtomString() }}</lastmod>
-            <changefreq>weekly</changefreq>
-            <priority>0.7</priority>
-        </url>
-    @endforeach
-
-    <!-- City Title Guide URLs -->
-    @php
-        $cities = [
-            'addison-texas',
-            'balch-springs',
-            'carrollton',
-            'cedar-hill',
-            'cockrell-hill',
-            'combine',
-            'coppell',
-            'dallas',
-            'desoto',
-            'duncan',
-            'farmers-branch',
-            'ferris',
-            'garland',
-            'glenn-heights',
-            'grand-prairie',
-            'highlandpark',
-            'hutchins',
-            'irving',
-            'lancaster',
-            'lewisville',
-            'mesquite',
-            'oakcliff',
-            'richardson',
-            'rowlett',
-            'seagoville',
-            'sunnyvale',
-        ];
-    @endphp
-
-    @foreach ($cities as $city)
-        <url>
-            <loc>{{ url('/how-to-get-your-title-in-' . $city) }}</loc>
-            <lastmod>{{ now()->toAtomString() }}</lastmod>
-            <changefreq>monthly</changefreq>
-            <priority>0.8</priority>
-        </url>
-    @endforeach
-    
     @php
         $locationCities = array_keys(config('fl_location_service_pages'));
     @endphp
